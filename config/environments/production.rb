8c7da523d563863d468b33c0a86bfcd2ddb040e5
Benoit Lafontaine <joel1di1@gmail.com>
2014-07-27 23:19:09 +0200

    fix mails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'martigua2.herokuapp.com' }
 
+  config.action_mailer.delivery_method   = :postmark
+  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
+
+
 end

d806b261fda001d6c209e4914f2cae8cc4c4b9e8
Benoit Lafontaine <joel1di1@gmail.com>
2015-09-12 11:05:17 +0200

    Add model spec on match selection

--- spec/models/match_selection_spec.rb
+++ spec/models/match_selection_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MatchSelection, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :match }
+  it { should belong_to :team }
+  it { should belong_to :user }
 end

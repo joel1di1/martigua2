f475e7432591396f7ad64827961c1f6976779721
Benoit Lafontaine <joel1di1@gmail.com>
2015-09-14 15:40:12 +0200

    Use AWS cloudfront as CDN in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-
+  config.action_controller.asset_host = "http://d1zljy12d9ls0t.cloudfront.net"
+  
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )

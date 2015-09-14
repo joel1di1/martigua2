71d61bbde93cefdef0d6a9435b5c271fe8becde9
Benoit Lafontaine <joel1di1@gmail.com>
2015-09-15 01:04:58 +0200

    use days for next matches

--- app/models/section.rb
+++ app/models/section.rb
@@ -65,8 +65,8 @@ class Section < ActiveRecord::Base
   def next_matches
     now = DateTime.now.at_beginning_of_week
     end_date = now.at_end_of_week + 2.weeks+2.days
-    Match.where('((start_datetime > ? AND start_datetime < ?) OR (prevision_period_start > ? AND prevision_period_start < ?))', 
-      now, end_date, now, end_date).where('local_team_id IN (?) OR visitor_team_id IN (?)', teams.map(&:id), teams.map(&:id))
+    Match.join_day.where('COALESCE(start_datetime, days.period_start_date) >= ? AND COALESCE(start_datetime, days.period_start_date) <= ?', 
+      now, end_date).date_ordered.where('local_team_id IN (?) OR visitor_team_id IN (?)', teams.map(&:id), teams.map(&:id))
   end
 
   def group_everybody

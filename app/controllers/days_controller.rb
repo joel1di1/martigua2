701509d0c15b5d1103393b47a7ca25eb6d66b8de
Benoit Lafontaine <joel1di1@gmail.com>
2017-08-28 11:33:37 +0200

    day controller with editable resources

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,28 +1,7 @@
-class DaysController < ApplicationController
-
-  respond_to :html, :json
-
-  def update
-    @day = Day.find(params[:id])
-
-    respond_to do |format|
-      if @day.update_attributes(day_params)
-        format.html { redirect_to(@day, :notice => 'Journée mise à jour.') }
-        format.json { respond_with_bip(@day) }
-      else
-        format.html { render :action => "edit" }
-        format.json { respond_with_bip(@day) }
-      end
-    end
-  end
+class DaysController < InheritedResources::Base
 
   protected
     def day_params
-      if params[:day]
-        params.require(:day).permit(:name)
-      else
-        {}
-      end
+      params.require(:day).permit(:name)
     end
-
 end

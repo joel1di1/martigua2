5bec105847031d242d722023e820da04761eb732
Benoit Lafontaine <joel1di1@gmail.com>
2017-08-28 10:55:42 +0200

    display day list in calendar view

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,6 @@
 class Calendar < ApplicationRecord
   belongs_to :season
+  has_many :days, inverse_of: :calendar
 
   validates_presence_of :season, :name
 
--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Calendar, type: :model do
   it { should belong_to :season }
+  it { should have_many :days }
   it { should validate_presence_of :season }
   it { should validate_presence_of :name }
 end

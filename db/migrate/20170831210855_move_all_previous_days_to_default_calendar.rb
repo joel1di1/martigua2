540d1e76efee4e6187b9cfaae16d51a95511f047
Benoit Lafontaine <joel1di1@gmail.com>
2017-08-31 23:41:48 +0200

    add default calendar to old seasons

---
+++ db/migrate/20170831210855_move_all_previous_days_to_default_calendar.rb
@@ -0,0 +1,18 @@
+class MoveAllPreviousDaysToDefaultCalendar < ActiveRecord::Migration[5.1]
+  DEFAULT_NAME = "*DEFAULT_CALENDAR_FOR_MIGRATION*"
+  def up
+    Season.all.each do |season|
+      if season.calendars.count == 0
+        calendar = Calendar.create! season: season, name: DEFAULT_NAME
+        season.championships.update_all(calendar_id: calendar.id)
+        season.days.update_all(calendar_id: calendar.id)
+      end
+    end
+  end
+  def down
+    calendars = Calendar.where(name: DEFAULT_NAME)
+    calendars.each do |calendar|
+      calendar.days.update_all(season_id: calendar.season_id)
+    end
+  end
+end

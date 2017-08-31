0348920276aa05e0bb25d5dfbab6c28a2b2e3a59
Benoit Lafontaine <joel1di1@gmail.com>
2017-09-01 01:14:09 +0200

    fix migration (remove column)

--- db/migrate/20170831210855_move_all_previous_days_to_default_calendar.rb
+++ db/migrate/20170831210855_move_all_previous_days_to_default_calendar.rb
@@ -1,5 +1,17 @@
+class Season < ActiveRecord::Base
+  has_many :days, inverse_of: :season, dependent: :destroy
+  has_many :championships, inverse_of: :season, dependent: :destroy
+  has_many :calendars, inverse_of: :season, dependent: :destroy
+end
+
+class Day < ActiveRecord::Base
+  belongs_to :calendar
+  belongs_to :season
+end
+
 class MoveAllPreviousDaysToDefaultCalendar < ActiveRecord::Migration[5.1]
   DEFAULT_NAME = "*DEFAULT_CALENDAR_FOR_MIGRATION*"
+
   def up
     Season.all.each do |season|
       if season.calendars.count == 0

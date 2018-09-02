b75941a70b53090567107f5169ad2a8634279d3b
Benoit Lafontaine <joel1di1@gmail.com>
2018-09-02 23:03:41 +0200

    clean code

--- app/controllers/participations_renewal_controller.rb
+++ app/controllers/participations_renewal_controller.rb
@@ -5,15 +5,22 @@ class ParticipationsRenewalController < ApplicationController
   end
 
   def create
-    players = params[:players_ids] ? User.find(params[:players_ids]) : []
-    coachs = params[:coachs_ids] ? User.find(params[:coachs_ids]) : []
-
-    players.each { |player| current_section.add_player!(player) }
-    coachs.each { |coach| current_section.add_coach!(coach) }
-
+    renew_players
+    renew_coachs
     redirect_to section_users_path(current_section)
-
   rescue ActiveRecord::RecordNotFound => e
     redirect_to section_participations_renewal_index_path(current_section), notice: "Erreur, un membre n'a pas été trouvé lors de son enregistrement, veuillez réessayer ou contacter JOE si ça continue à ne par marcher"
+  end
+
+  private
+
+  def renew_players
+    players = params[:players_ids] ? User.find(params[:players_ids]) : []
+    players.each { |player| current_section.add_player!(player) }
+  end
+
+  def renew_coachs
+    coachs = params[:coachs_ids] ? User.find(params[:coachs_ids]) : []
+    coachs.each { |coach| current_section.add_coach!(coach) }
   end
 end

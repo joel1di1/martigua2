5bfb69fec78095b1725e242fd61599fd89735289
Benoit Lafontaine <joel1di1@gmail.com>
2018-11-04 00:34:24 +0100

    add drag and drop on selection

--- app/assets/javascripts/selection.js
+++ app/assets/javascripts/selection.js
@@ -19,4 +19,24 @@ $(document).on('turbolinks:load', function(){
   $("#mail-compo").click(function() {
     $('#mail-compo').selectText();
   });
+
+  $('.player-draggable').draggable({
+    revert: true,
+    revertDuration: 200,
+    zIndex: 100,
+  });
+
+  $('.team-droppable').droppable({
+    activate: function(event, ui) {},
+    drop: function( event, ui ) {
+        let playerId = ui.draggable.data('player_id');
+        let matchId = $(this).data('match_id');
+        let teamId = $(this).data('team_id');
+        console.log(playerId);
+        console.log(matchId);
+        console.log(teamId);
+
+        $('form#mtp-' + matchId + '-' + teamId + '-' + playerId).submit();
+    },
+  });
 });

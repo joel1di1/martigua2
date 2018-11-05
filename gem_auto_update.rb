c1c1a45d08aa1452955b6e010358121c4cca1a2d
Benoit Lafontaine <joel1di1@gmail.com>
2018-11-05 01:52:54 +0100

    update auto update gems

--- gem_auto_update.rb
+++ gem_auto_update.rb
@@ -3,25 +3,51 @@
 # Usage:
 # > ruby gem_auto_update.rb
 
-bundle_outdated_result = `bundle outdated`
-gem_names = bundle_outdated_result.split("\n").map{|s| /^\s*\*\s([^\s]*)/.match(s)&.send(:[], 1) }.compact
+# try to update everything
 
-gem_names.each do |gem_name|
-  puts "-- reset Gemfile.lock"
-  system "git checkout Gemfile.lock"
-  puts "-- bundle update #{gem_name}"
-  if system "bundle update #{gem_name}"
-    if system 'git diff-index --quiet HEAD'
-      puts "no modification, do not update #{gem_name}"
+
+def log(msg)
+  puts "=== #{msg}\n"
+end
+
+def exec_system(cmd, desc: nil)
+  _exec(cmd, desc: nil, mode: :system)
+end
+
+def exec_thick(cmd, desc: nil)
+  _exec(cmd, desc: nil, mode: :thick)
+end
+
+def _exec(cmd, desc: nil, mode: )
+  log("execute `#{cmd}` (using #{mode} mode) -- #{desc}")
+  mode == :thick ? `#{cmd}` : system(cmd)
+end
+
+def update_test_and_commit(gem_name: nil)
+  if exec_system "bundle update #{gem_name}"
+    if exec_system 'git diff-index --quiet HEAD', desc: 'check if any modification'
+      log("no modification, do not update #{gem_name || 'all'}")
     else
-      puts '-- try bundle exec rspec'
-      if system "bundle exec rspec"
-        puts 'commit gem update'
-        system 'git add Gemfile.lock'
-        system "git commit -m 'auto update #{gem_name} gem'"
+      if exec_system "bundle exec rspec", desc: 'run tests'
+        exec_system 'git add Gemfile.lock'
+        exec_system "git commit -m 'auto update #{gem_name || 'all'}'"
       else
-        puts "error, do not update #{gem_name}"
+        log("error, do not update #{gem_name || 'all'}")
       end
     end
   end
+end
+
+
+exec_system 'git checkout Gemfile.lock'
+exec_system 'bundle update'
+
+update_test_and_commit
+
+bundle_outdated_result = exec_thick 'bundle outdated'
+gem_names = bundle_outdated_result.split("\n").map{|s| /^\s*\*\s([^\s]*)/.match(s)&.send(:[], 1) }.compact
+
+gem_names.each do |gem_name|
+  exec_system 'git checkout Gemfile.lock'
+  update_test_and_commit(gem_name: gem_name)
 end

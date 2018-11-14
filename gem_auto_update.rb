5c12015a5cef45f71bd61c9e79b1c84411bb9fd4
Benoit Lafontaine <joel1di1@gmail.com>
2018-11-14 02:12:38 +0100

    return if bundle update passed

--- gem_auto_update.rb
+++ gem_auto_update.rb
@@ -26,13 +26,16 @@ def update_test_and_commit(gem_name: nil)
   if exec_system "bundle update #{gem_name}"
     if exec_system 'git diff-index --quiet HEAD', desc: 'check if any modification'
       log("no modification, do not update #{gem_name || 'all'}")
+      true
     else
       if exec_system "bundle exec rspec", desc: 'run tests'
         exec_system 'git add Gemfile.lock'
         exec_system "git commit -m 'auto update #{gem_name || 'all'}'"
+        true
       else
         log("error, do not update #{gem_name || 'all'}")
         exec_system 'git checkout Gemfile.lock', desc: 'Revert Gemfile.lock'
+        false
       end
     end
   end
@@ -41,7 +44,9 @@ end
 exec_system 'git checkout Gemfile.lock'
 exec_system 'bundle update'
 
-update_test_and_commit
+if update_test_and_commit
+  log 'Bundle update successful, stop here'
+end
 
 bundle_outdated_result = exec_thick 'bundle outdated'
 gem_names = bundle_outdated_result.split("\n").map { |s| /^\s*\*\s([^\s]*)/.match(s)&.send(:[], 1) }.compact

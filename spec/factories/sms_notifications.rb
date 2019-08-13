78b61aab6ceb8c851db69214ddc073071a80a778
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2019-08-13 08:56:50 +0200

    Faker use named params

--- spec/factories/sms_notifications.rb
+++ spec/factories/sms_notifications.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :sms_notification do
-    title { Faker::Lorem.sentence(4) }
-    description { Faker::Lorem.sentence(20) }
+    title { Faker::Lorem.sentence(word_count: 4) }
+    description { Faker::Lorem.sentence(word_count: 20) }
     section
   end
 end

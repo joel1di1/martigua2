1f45089d23ac51a7fa957103256affbaf375d3ad
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2019-08-16 13:47:10 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -341,31 +341,31 @@ ActiveRecord::Schema.define(version: 20190815120810) do
     t.index ["location_id"], name: "index_trainings_on_location_id"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: ""
-    t.string "reset_password_token", limit: 255
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: ""
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip", limit: 255
-    t.string "last_sign_in_ip", limit: 255
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "first_name", limit: 255
-    t.string "last_name", limit: 255
-    t.string "nickname", limit: 255
-    t.string "phone_number", limit: 255
-    t.string "authentication_token", limit: 255
-    t.string "invitation_token", limit: 255
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
+    t.string "phone_number"
+    t.string "authentication_token"
+    t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer "invitation_limit"
+    t.string "invited_by_type"
     t.integer "invited_by_id"
-    t.string "invited_by_type", limit: 255
     t.integer "invitations_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true

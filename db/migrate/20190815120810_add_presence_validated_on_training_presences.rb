4a745413576855f49dbfaca5fe7c59f53a55d170
Benoit Lafontaine <joel1di1@gmail.com>
2019-08-28 18:47:28 +0200

    rubocop

--- db/migrate/20190815120810_add_presence_validated_on_training_presences.rb
+++ db/migrate/20190815120810_add_presence_validated_on_training_presences.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPresenceValidatedOnTrainingPresences < ActiveRecord::Migration[5.1]
   def change
     add_column :training_presences, :presence_validated, :boolean

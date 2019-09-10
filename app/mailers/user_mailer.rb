058b5eace6c5d93a52efbd0ee58d374497b5997f
Benoit Lafontaine <joel1di1@gmail.com>
2019-09-10 19:26:55 +0200

    correct mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,6 +23,7 @@ class UserMailer < ActionMailer::Base
     @user = next_training_duties.first
 
     subject = "Chasubles, c'est ton tour : (#{training.start_datetime.strftime("%-d/%-m")})"
-    mail to: @user.email, cc: next_training_duties[1, -1].map(&:email), subject: subject
+    ccs =  next_training_duties[1..-1].map(&:email) + ['joel1di1@gmail.com', 'guillaume.pech@gmail.com']
+    mail to: @user.email, cc: ccs, subject: subject
   end
 end

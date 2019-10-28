c060e44c0213db2a4c25b952afc3992e9599139b
Florent Beaurain <beaurain.florent@gmail.com>
2019-10-28 21:03:15 +0100

    bump rails load_defaults from 5.0 to 5.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   before_action :authenticate_user_from_token!, except: :catch_404
   before_action :authenticate_user!, except: :catch_404
   before_action :set_raven_context

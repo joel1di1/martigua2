324e5a8f2c6e8f9b1c0e9666e542e74aebb09ca3
Benoit Lafontaine <joel1di1@gmail.com>
2019-11-10 21:03:25 +0100

    rubocop

--- spec/features/groups/see_group_detail_spec.rb
+++ spec/features/groups/see_group_detail_spec.rb
@@ -12,17 +12,17 @@ describe 'See group detail' do
   end
 
   it 'visit the group page with 2 groups' do
-    group_1 = create :group, section: section
-    group_2 = create :group, section: section
+    group1 = create :group, section: section
+    group2 = create :group, section: section
 
-    group_1.add_user!(user)
+    group1.add_user!(user)
     click_link 'Groupes'
 
-    expect(page).to have_content group_1.name
-    expect(page).to have_content group_2.name
+    expect(page).to have_content group1.name
+    expect(page).to have_content group2.name
     expect(page).to have_content section.group_everybody.name
 
-    click_link group_1.name
-    expect(page).to have_content group_1.name
+    click_link group1.name
+    expect(page).to have_content group1.name
   end
 end

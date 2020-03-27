89bfb96690f4aadbbe144643cd31f58517323fb4
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2020-03-27 14:11:36 +0100

    heroku information on ping

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -12,8 +12,10 @@ class VisitorsController < ApplicationController
   def ping
     map = {
       'hostname' => Socket.gethostname,
-      'revision' => revision,
-      'app' => 'export_tools',
+      'revision' => ENV['HEROKU_SLUG_COMMIT'],
+      'version' => ENV['HEROKU_RELEASE_VERSION'],
+      'created_at' => ENV['HEROKU_RELEASE_CREATED_AT'],
+      'app' => ENV['HEROKU_APP_NAME'],
     }
     render json: map
   end

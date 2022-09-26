0fa30f109de900b6c1fe8aae0739617580e30144
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-09-26 18:16:44 +0200

    use rails 7.0

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,7 @@ module Martigua2
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
-    # remove this line when config.load_defaults 7.0
-    config.active_support.cache_format_version
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #

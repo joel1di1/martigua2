f95ee46ba616f7c41440f10c7e9a7f9fca54d52c
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-09-27 11:39:16 +0200

    quick fix presence en prod

--- app/controllers/training_presences_controller.rb
+++ app/controllers/training_presences_controller.rb
@@ -17,4 +17,9 @@ class TrainingPresencesController < ApplicationController
 
     user._confirm_presence!(training, params[:present].present?)
   end
+
+  def confirm_presence_get
+    current_user.present_for! Training.find(params[:training_id])
+    redirect_with(fallback: root_path)
+  end
 end

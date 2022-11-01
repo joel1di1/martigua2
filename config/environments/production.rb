40b4b94fe72a06522592d6b1e8c4501973553c1d
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-11-01 18:18:35 +0100

    try cache assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,15 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://d27sj2p8c08vei.cloudfront.net'
 
+  # This will affect assets served from /app/assets
+  config.static_cache_control = 'public, max-age=31536000'
+
+  # This will affect assets in /public, e.g. webpacker assets.
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000',
+    'Expires' => 1.year.from_now.to_fs(:rfc822)
+  }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX

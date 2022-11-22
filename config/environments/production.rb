576818949bb4c71528f02d8dbf8b92bde78696be
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-11-22 01:45:34 +0100

    remove delayed job from the prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', nil) }
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :delayed_job
   # config.active_job.queue_name_prefix = "martigua2_production"
 
   config.action_mailer.perform_caching = false

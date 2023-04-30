54146fa166f19508de1448487a54a67117a5900e
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-05-01 00:22:58 +0200

    fix devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,26 +1,8 @@
 # frozen_string_literal: true
 
-class TurboFailureApp < Devise::FailureApp
-  def respond
-    if request_format == :turbo_stream
-      redirect
-    else
-      super
-    end
-  end
-
-  def skip_format?
-    %w[html turbo_stream */*].include? request_format.to_s
-  end
-end
-
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # Configure the parent class to the custom controller.
-  # used for turbo
-  config.parent_controller = 'TurboDeviseUserController'
-  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -295,9 +277,9 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  config.warden do |manager|
-    manager.failure_app = TurboFailureApp
-  end
+  # config.warden do |manager|
+  #   manager.failure_app = TurboFailureApp
+  # end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine

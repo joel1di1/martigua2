f316b428b28dd6dfb44131dad3fa127fc277c13a
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-05-06 00:30:50 +0200

    service worker js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.version = '1.0'
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile +=  %w( service_worker.js )
+Rails.application.config.assets.precompile +=  %w( service_workers_register.js )

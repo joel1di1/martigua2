fa91f9917deae3f5403ec1356f1e2610ab2249b2
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-06-17 02:28:27 +0200

    better spec

--- spec/features/channels/channels_index_spec.rb
+++ spec/features/channels/channels_index_spec.rb
@@ -4,10 +4,11 @@
 describe 'channels index' do
   let(:section) { create(:section) }
   let(:user) { create(:user, with_section: section) }
+  let!(:random_chan) { create(:channel, section: section) }
 
   before do
     signin_user user, close_notice: true
-    random_chan = create(:channel, section: section)
+
   end
 
   context 'on small screen' do
@@ -21,10 +22,10 @@ describe 'channels index' do
       expect(page).to have_css '#channels-list a', count: 2
 
       # expects to see general channel
-      expect(page).to have_css '#channels-list a', text: 'general'
+      expect(page).to have_css '#channels-list a', text: 'Général'
 
       # expects to see random channel
-      expect(page).to have_css '#channels-list a', text: 'random'
+      expect(page).to have_css '#channels-list a', text: random_chan.name
     end
   end
 end

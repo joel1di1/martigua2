2270c7138c08d12c9a1300ce07b1c5af313b8adf
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-09-01 23:25:06 +0200

    freeze time for duty tests

--- spec/models/duty_task_spec.rb
+++ spec/models/duty_task_spec.rb
@@ -12,18 +12,20 @@ RSpec.describe DutyTask do
     let(:current_season_end) { Date.new(Time.zone.today.year + 1, 6, 30) }
 
     it 'returns duty tasks for the current season' do
-      # Create duty tasks for the current season
-      current_season_tasks = [
-        create(:duty_task, realised_at: current_season_start),
-        create(:duty_task, realised_at: current_season_end),
-        create(:duty_task, realised_at: current_season_start + 1.month),
-        create(:duty_task, realised_at: current_season_end - 1.month)
-      ]
+      Timecop.freeze(Time.zone.local(Time.zone.today.year, 12, 1)) do
+        # Create duty tasks for the current season
+        current_season_tasks = [
+          create(:duty_task, realised_at: current_season_start),
+          create(:duty_task, realised_at: current_season_end),
+          create(:duty_task, realised_at: current_season_start + 1.month),
+          create(:duty_task, realised_at: current_season_end - 1.month)
+        ]
 
-      # Create duty tasks for other seasons
+        # Create duty tasks for other seasons
 
-      # Call the scope and check if it returns the correct records
-      expect(DutyTask.for_current_season).to match_array(current_season_tasks)
+        # Call the scope and check if it returns the correct records
+        expect(DutyTask.for_current_season).to match_array(current_season_tasks)
+      end
     end
   end
 

f44d037d582311159e631987bab274da09297bb7
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-09-24 19:03:58 +0200

    use same chrome driver for E2E tests

--- spec/support/chrome.rb
+++ spec/support/chrome.rb
@@ -1,24 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.configure do |_config|
-  # NOTE: Capybara registers this by default
   Capybara.register_driver :selenium_chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-  end
-
-  Capybara.register_driver :selenium_chrome_headless do |app|
     options = Selenium::WebDriver::Chrome::Options.new
-
-    options.add_argument('--headless')
+    options.add_argument('--headless') unless ENV['SHOW_BROWSER'].present?
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1280,800')
 
     Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
   end
 
-  if ENV['SHOW_BROWSER'].present?
-    Capybara.current_driver = :selenium_chrome
-  else
-    Capybara.current_driver = :selenium_chrome_headless
-  end
+  Capybara.current_driver = :selenium_chrome
 end

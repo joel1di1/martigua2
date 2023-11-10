143bfcfe8ca73e058b5d3f5f3ed24a20c9d94e2d
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-11-10 09:02:00 +0100

    config sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,9 +6,9 @@ Sentry.init do |config|
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
+  config.traces_sample_rate = 0.1
   # or
-  config.traces_sampler = lambda do |_context|
-    true
-  end
+  # config.traces_sampler = lambda do |_context|
+  #   true
+  # end
 end

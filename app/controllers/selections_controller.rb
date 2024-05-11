3368ae8f1c90221dd787fbcb3be623f17c3ce2bd
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-05-11 23:48:15 +0200

    refacto selection controller

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -8,37 +8,43 @@ class SelectionsController < ApplicationController
     @day_selections = Selection.joins(match: :day).where(matches: { day_id: @day.id }).includes(:user)
     @users_already_selected = @day_selections.map(&:user).uniq
 
-    @available_players = Set.new
-    @non_available_players = Set.new
-
     matches = @teams_with_matches.map(&:second)
     @players = current_section.players
 
-    @availabilities_by_user_and_match = {}
-    @players.each do |player|
-      @availabilities_by_user_and_match[player.id] = {}
-      matches.each { |match| @availabilities_by_user_and_match[player.id][match.id] = nil }
-    end
+    @availabilities_by_user_and_match = availabilities_by_user_and_match(@players, matches)
+
+    @available_players, @non_available_players = prepare_availabilities(matches, @availabilities_by_user_and_match)
+
+    @available_players = (@available_players - @users_already_selected).to_a.sort_by(&:short_name)
+    @non_available_players = (@non_available_players - @available_players - @users_already_selected).to_a.sort_by(&:short_name)
+    @no_response_players = (@players - @available_players - @non_available_players - @users_already_selected).to_a.sort_by(&:short_name)
+
+    @last_trainings, @presences_by_user_and_training = prepare_training_presences(current_section, @players)
+  end
+
+  def prepare_availabilities(matches, availabilities_by_user_and_match)
+    available_players = Set.new
+    non_available_players = Set.new
 
     availabilities = MatchAvailability.includes(:user).where(match: matches)
     availabilities.each do |availability|
-      if @availabilities_by_user_and_match[availability.user_id]
-        @availabilities_by_user_and_match[availability.user_id][availability.match_id] =
+      if availabilities_by_user_and_match[availability.user_id]
+        availabilities_by_user_and_match[availability.user_id][availability.match_id] =
           availability.available
       end
-      (availability.available ? @available_players : @non_available_players) << availability.user
+      (availability.available ? available_players : non_available_players) << availability.user
     end
 
-    @available_players = (@available_players - @users_already_selected).to_a
-    @available_players.sort! { |a, b| a.short_name <=> b.short_name }
+    [available_players, non_available_players]
+  end
 
-    @non_available_players = (@non_available_players - @available_players - @users_already_selected).to_a
-    @non_available_players.sort! { |a, b| a.short_name <=> b.short_name }
-
-    @no_response_players = (@players - @available_players - @non_available_players - @users_already_selected).to_a
-    @no_response_players.sort! { |a, b| a.short_name <=> b.short_name }
-
-    @last_trainings, @presences_by_user_and_training = prepare_training_presences(current_section, @players)
+  def availabilities_by_user_and_match(players, matches)
+    availabilities_by_user_and_match = {}
+    players.each do |player|
+      availabilities_by_user_and_match[player.id] = {}
+      matches.each { |match| availabilities_by_user_and_match[player.id][match.id] = nil }
+    end
+    availabilities_by_user_and_match
   end
 
   def destroy

fb51d280e9ce21ce93fb0ccfb30d3b1e936b4d45
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-09-13 00:10:29 +0200

    absence for all matches

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -4,6 +4,8 @@ class Absence < ApplicationRecord
   MOTIVES = %w[Blessure Maladie Perso Travail Autre].freeze
 
   validates :name, presence: true, inclusion: { in: MOTIVES }
+  validates :start_at, presence: true
+  validates :end_at, presence: true
 
   after_save :update_training_presences
   after_save :update_match_availabilities
@@ -17,7 +19,11 @@ class Absence < ApplicationRecord
   end
 
   def update_match_availabilities
-    matchs = Match.with_start_between(start_at, end_at)
-    user.not_available_for!(matchs)
+    user_teams = Team.joins(:sections).where(sections: user.sections)
+    section_matchs = Match.where(local_team: user_teams).or(Match.where(visitor_team: user_teams))
+    matchs = section_matchs.with_start_between(start_at, end_at)
+    other_matchs = section_matchs.where(start_datetime: nil).includes(:day).where(day: { period_start_date: start_at..end_at, period_end_date: start_at..end_at })
+
+    user.not_available_for!(matchs + other_matchs)
   end
 end

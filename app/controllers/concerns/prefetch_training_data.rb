629bb6ca2651a3642c2c74975397b572b8aa5918
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-09-12 22:01:52 +0200

    remove comment

--- app/controllers/concerns/prefetch_training_data.rb
+++ app/controllers/concerns/prefetch_training_data.rb
@@ -2,7 +2,6 @@
 
 module PrefetchTrainingData
   def add_training_prefetch_data(trainings)
-    # `SELECT "training_presences".* FROM "training_presences" WHERE "training_presences"."training_id" = $1 AND "training_presences"."is_present" = $2;`
     @nb_presents = TrainingPresence.where(training: trainings, is_present: true).group(:training).count
     @nb_not_presents = TrainingPresence.where(training: trainings, is_present: false).group(:training).count
     @nb_no_response = trainings.index_with do |training|

e3d2bfaa5f8aea9aefeaef7983963a31863c606c
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-10-27 11:27:35 +0100

    fix redis certif

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
-$redis = Redis.new(url: ENV.fetch('REDIS_URL', 'redis://localhost:63791/0')) # rubocop:disable Style/GlobalVars
+$redis = Redis.new( # rubocop:disable Style/GlobalVars
+  url: ENV.fetch('REDIS_URL', 'redis://localhost:63791/0'),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)

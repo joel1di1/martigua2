a735631e4870b609e9dc95d5ab4a9e278f27d40c
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-04-21 15:21:42 +0200

    Migrate sections controller spec to request spec

---
+++ spec/requests/sections_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Sections', type: :request do
+  let(:club) { create(:club) }
+  let(:user) { create(:user) }
+  let(:section) { create(:section, club:) }
+
+  describe 'GET /clubs/:club_id/sections/new' do
+    before do
+      get new_club_section_path(club), params: { user_email: user.email, user_token: user.authentication_token }
+    end
+
+    it { expect(response).to have_http_status(:success) }
+    # We can't test assigns in request specs, but we could test that the rendered content includes relevant information
+  end
+
+  describe 'POST /clubs/:club_id/sections' do
+    let(:section_attributes) { attributes_for(:section, club: nil) }
+    let(:auth_params) do
+      { user_email: user.email, user_token: user.authentication_token }
+    end
+
+    before do
+      post club_sections_path(club), params: auth_params.merge(section: section_attributes), as: :json
+    end
+
+    it { expect(response).to have_http_status(:created) }
+    
+    it "makes the user a coach of the section" do
+      expect(user).to be_coach_of(Section.where(club:, name: section_attributes[:name]))
+    end
+  end
+
+  describe 'GET /sections/:id' do
+    before do
+      sign_in user
+      get section_path(section)
+    end
+
+    it { expect(response).to have_http_status(:success) }
+    # Can't test assigns in request specs, but we can verify the response contains expected data
+    # We could test specific content if needed
+  end
+
+  # Note: The #suggested_user_stat method test is a private method test that doesn't translate directly
+  # to a request spec. These types of tests are typically handled differently:
+  # 1. Test the observable behaviors through public endpoints
+  # 2. Extract the method to a service object that can be tested directly
+  # 3. Move the method to a model or concern where it makes more sense
+end

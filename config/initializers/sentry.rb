73292bb77f7e4b88d28eb88506891ef21d80a184
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-04-21 23:00:43 +0200

    set trace sample rate differently in dev than in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,7 +6,9 @@ Sentry.init do |config|
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.25
+
+  config.traces_sample_rate = Rails.env.development? ? 0.0 : 0.25
+
   # or
   # config.traces_sampler = lambda do |_context|
   #   true

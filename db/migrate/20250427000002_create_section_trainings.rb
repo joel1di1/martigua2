57c30b5e865d74bd8b18be4d764d45fc758cc593
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-04-27 22:23:27 +0200

    rubocop

--- db/migrate/20250427000002_create_section_trainings.rb
+++ db/migrate/20250427000002_create_section_trainings.rb
@@ -5,23 +5,23 @@ class CreateSectionTrainings < ActiveRecord::Migration[6.1]
     create_table :section_trainings do |t|
       t.references :section, null: false, foreign_key: true
       t.references :training, null: false, foreign_key: true
-      
+
       t.timestamps
     end
-    
+
     # Add a unique index to prevent duplicate associations
-    add_index :section_trainings, [:section_id, :training_id], unique: true
-    
+    add_index :section_trainings, %i[section_id training_id], unique: true
+
     # Copy data from sections_trainings to section_trainings
     reversible do |dir|
       dir.up do
-        execute <<-SQL
+        execute <<-SQL.squish
           INSERT INTO section_trainings (section_id, training_id, created_at, updated_at)
           SELECT section_id, training_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM sections_trainings
         SQL
       end
     end
-    
+
     # Don't drop the old table yet - do it in another migration after confirming everything works
     # drop_table :sections_trainings
   end

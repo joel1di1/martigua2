5d9566453318b445ff417142529a8b819fcb6133
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-04-27 22:03:02 +0200

    rubocop

--- db/migrate/20250427000001_create_group_trainings.rb
+++ db/migrate/20250427000001_create_group_trainings.rb
@@ -5,23 +5,23 @@ class CreateGroupTrainings < ActiveRecord::Migration[6.1]
     create_table :group_trainings do |t|
       t.references :group, null: false, foreign_key: true
       t.references :training, null: false, foreign_key: true
-      
+
       t.timestamps
     end
-    
+
     # Add a unique index to prevent duplicate associations
-    add_index :group_trainings, [:group_id, :training_id], unique: true
-    
+    add_index :group_trainings, %i[group_id training_id], unique: true
+
     # Copy data from groups_trainings to group_trainings
     reversible do |dir|
       dir.up do
-        execute <<-SQL
+        execute <<-SQL.squish
           INSERT INTO group_trainings (group_id, training_id, created_at, updated_at)
           SELECT group_id, training_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM groups_trainings
         SQL
       end
     end
-    
+
     # Don't drop the old table yet - do it in another migration after confirming everything works
     # drop_table :groups_trainings
   end

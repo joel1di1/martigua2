399aebcc0db25c26688dfffdd97483ce8174af10
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-05-23 22:04:23 +0200

    config.assets.compile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.assets.compile = false
+
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 

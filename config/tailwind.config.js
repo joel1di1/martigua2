63a0832b55aa437b84f80eea87757be0e1acc799
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2025-06-17 11:51:09 +0200

    migrate tailwind to 4.2, config not working

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,29 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// const defaultTheme = require('tailwindcss/defaultTheme')
 
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'class',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        teal: colors.teal,
-        cyan: colors.cyan,
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
   ]
 }

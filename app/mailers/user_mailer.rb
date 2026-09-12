29711caac20bdf57cb947a5c12ccde8511b1e960
BLA <benoit.lafontaine@clickandboat.com>
2026-09-12 18:24:29 +0200

    dead code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -39,19 +39,4 @@ class UserMailer < ApplicationMailer
     @token = user.generate_token_for(:email_authentication)
     mail to: recipient, subject: "Tu peux maintenant répondre pour #{user.full_name}"
   end
-
-  # TODO: factorize method missing with ApplicationRecord
-  def self.method_missing(method, *, &block)
-    if method.to_s.start_with?('async_')
-      raise 'async jobs with block are not supported' if block.present?
-
-      ActiveRecordAsyncJob.perform_async(name, nil, method.to_s.sub('async_', ''), *)
-    else
-      super
-    end
-  end
-
-  def self.respond_to_missing?(method, include_private = false)
-    method.to_s.start_with?('async_') || super
-  end
 end
